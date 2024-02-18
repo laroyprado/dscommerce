934f54fd5b2a71ddd6f02e2e48f978fc5a126e99
laroy prado <laroyprado@gmail.com>
2024-02-18 18:50:08 -0300

    feat: cria entidade OrderItemPK

---
+++ src/main/java/com/project/dscommerce/entities/OrderItemPK.java
@@ -0,0 +1,30 @@
+package com.project.dscommerce.entities;
+
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Embeddable
+public class OrderItemPK {
+
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+
+
+}
