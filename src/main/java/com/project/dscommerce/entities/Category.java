7fc82c5246aa8e567b0fb66fb073aa232cd1994f
laroy prado <laroyprado@gmail.com>
2024-02-18 18:49:44 -0300

    feat: cria entidade category

---
+++ src/main/java/com/project/dscommerce/entities/Category.java
@@ -0,0 +1,32 @@
+package com.project.dscommerce.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tb_category")
+public class Category {
+
+
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
+    @Id
+    private Long id;
+
+
+    private String name;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Product> products = new HashSet<>();
+
+}
