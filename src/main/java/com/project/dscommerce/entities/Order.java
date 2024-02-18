7023fb6355ed1564ced9c9d996e638405b825e42
laroy prado <laroyprado@gmail.com>
2024-02-18 18:49:52 -0300

    feat: cria entidade order

--- src/main/java/com/project/dscommerce/entities/Order.java
+++ src/main/java/com/project/dscommerce/entities/Order.java
@@ -6,6 +6,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 
 @AllArgsConstructor
@@ -30,6 +32,10 @@ public class Order {
 
     @OneToOne(mappedBy = "order",cascade = CascadeType.ALL)
     private Payment payment;
+
+
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
 
 
 
